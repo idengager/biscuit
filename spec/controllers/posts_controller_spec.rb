e9bd0be0789776fa191fbfd839eff22b38c1ab93
Kasia Jarmołkowicz <kasia.jarmolkowicz@gmail.com>
2016-01-06 22:02:43 +0100

    WIP Show image properly

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,28 +1,37 @@
 require "rails_helper"
 
 describe PostsController do
-  it "responds with HTTP 200" do
-    get :latest_post_url, token: ""
-    expect(response).to be_ok
-  end
-
-  it "renders plain text" do
-    get :latest_post_url, token: ""
-    expect(response.content_type).to eq("text/plain")
-    expect(response.body).not_to eq("Sucharek nr 1")
-  end
-
   context "when slack token is valid" do
+    it "responds with HTTP 200" do
+      get :latest_post_url, token: Rails.application.secrets.slack_token
+      expect(response).to be_ok
+    end
+
+    it "renders json" do
+      get :latest_post_url, token: Rails.application.secrets.slack_token
+      expect(response.content_type).to eq("application/json")
+    end
+
     it "renders correct content" do
       get :latest_post_url, token: Rails.application.secrets.slack_token
-      expect(response.body).to eq("Sucharek nr 1")
+      response_hash = JSON.parse(response.body)
+
+      expect(response_hash).to have_key("response_type")
+      expect(response_hash).to have_key("unfurl_media")
+      expect(response_hash).to have_key("attachments")
     end
   end
 
   context "when slack token is invalid" do
+    it "responds with HTTP 200" do
+      get :latest_post_url, token: "4321"
+      expect(response).to be_ok
+    end
+
     it "renders nothing" do
       get :latest_post_url, token: "1234"
-      expect(response.body).not_to eq("Sucharek nr 1")
+      expect(response.content_type).to eq("text/plain")
+      expect(response.body).to be_empty
     end
   end
 end
